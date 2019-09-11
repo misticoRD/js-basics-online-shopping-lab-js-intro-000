03623ae0045dbee1a6a84e17090816876286de60
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-11 22:14:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,7 +39,10 @@ function viewCart() {
 
 
 function total() {
-  // write your code here
+let total = 0;
+  for ( let i = 0; i < cart.length; i++) {
+    total += cart[i].itemPrice;
+  }
 }
 
 function removeFromCart(item) {
