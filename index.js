a892b19bc792c7764a79698bea7454087f8fd285
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-11 22:20:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,7 +47,11 @@ let total = 0;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for ( let i = 0; i < cart.length; i++) {
+    if(cart[i].itemName === item){
+      delete cart[i].itemName;
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
