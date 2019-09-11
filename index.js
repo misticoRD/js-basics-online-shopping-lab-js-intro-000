2c67062b80e516195992998886f250ef59bda556
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-11 21:01:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,16 @@ let price = Math.floor(Math.random() * 100) + 1;
 }
 
 function viewCart() {
-  if(cart.length === 0){
-    return "Your shopping cart is empty.";
-  }else{
-    return `In your cart, you have ${item} at ${price}.`
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  } else {
+    var items = [];
+    for (var i = 0; i < cart.length; i++) {
+      for (var item in cart[i]) {
+        items.push(item + " at $" + cart[i][item])
+      }
+    }
+    console.log("In your cart, you have " + items.join(", ") + ".");
   }
 }
 
